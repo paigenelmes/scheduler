122b664514973709c2434866f882aeca5ed2dabc
Paige Nelmes <designer@paigenelmes.com>
2023-01-25 19:11:21 -0800

    Add & test formatSpots function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,9 +8,23 @@ export default function DayListItem(props) {
     "day-list__item--full": !props.spots
   });
 
+  const formatSpots = () => {
+    let spotMessage = `${props.spots} spots remaining`;
+    
+    if (props.spots === 1) {
+      spotMessage = `${props.spots} spot remaining`;
+    } 
+    if (props.spots === 0) {
+      spotMessage = "no spots remaining";
+    }
+  
+    return spotMessage;
+  };
+
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className={"text--regular"}>{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>    </li>
+      <h3 className="text--light">{formatSpots()}</h3>
+    </li>
   );
 }
