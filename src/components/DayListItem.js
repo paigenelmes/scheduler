aaea3cd340055a64bc97108529716bdc7bee5d0b
Paige Nelmes <designer@paigenelmes.com>
2023-01-25 18:27:44 -0800

    Fix onClick in DayListItem component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,12 +2,8 @@ import React from 'react';
 
 export default function DayListItem(props) {
   return (
-    <li>
-      <h2 className="text--regular"
-      onClick={() => props.setDay(props.name)}
-      >
-        {props.name}
-      </h2>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
