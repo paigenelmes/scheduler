7c8d99c4e6e5571db7fa84e6085317041cfcc03d
Paige Nelmes <designer@paigenelmes.com>
2023-02-01 16:45:25 -0800

    Add axios and API request for days

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,31 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "components/Appointment/index";
 import { appointments } from "components/Appointment/index";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
   //Hooks
   const [value, onChange] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  //GET request to /api/days using axios
+
+  useEffect(() => {
+    const daysAPI = "http://localhost:8001/api/days";
+    axios.get(daysAPI).then(response => {
+      setDays([...response.data]);
+    });
+    }, [])
 
   //Helper function that converts the appointments object to an array and maps over the array
   //Returns a spread object with props for the appointment list
