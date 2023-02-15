48bbd2f07e7b52d89e8a535b0fb224a806b9fad7
Paige Nelmes <designer@paigenelmes.com>
2023-02-15 13:51:25 -0800

    Add prop-types library

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList(props) {
 
@@ -15,6 +16,10 @@ export default function InterviewerList(props) {
       />
     );
   });
+
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
 
   return (
     <section className="interviewers">
