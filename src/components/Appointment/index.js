d76b0b63d0c35ef386c66ee95af68129b9ec7ded
Paige Nelmes <designer@paigenelmes.com>
2023-02-27 11:36:50 -0800

    Update error modes to go back on close

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -94,10 +94,18 @@ export default function Appointment(props) {
         />
       )}
       {mode === ERROR_DELETE && (
-        <Error message={"Could not cancel appointment."} />
+        <Error 
+          message={"Could not cancel appointment."} 
+          onClose={(() => back())}
+        />
       )}
 
-      {mode === ERROR_SAVE && <Error message={"Could not save appointment."} />}
+      {mode === ERROR_SAVE && (
+      <Error 
+        message={"Could not save appointment."} 
+        onClose={(() => back())}
+      />
+      )}
     </article>
   );
 }
